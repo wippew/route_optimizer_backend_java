a0555ed161df1cd5e93c5fafa1113dbcb621630a
wippew <victor.pawals@gmail.com>
2022-12-13 18:22:07 +0200

    the rest service now works!

--- src/main/java/DG/DA/MaintenanceController.java
+++ src/main/java/DG/DA/MaintenanceController.java
@@ -2,17 +2,30 @@ package DG.DA;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class MaintenanceController {
 
 	@GetMapping("/employees")
-	JSONObject all() {
-		JSONObject jo = new JSONObject();
-		jo.put("name", "jon doe");
-		jo.put("age", "22");
-		jo.put("city", "chicago");
-		return jo;
+	public Map<String, String> sayHello() {
+	    HashMap<String, String> map = new HashMap<>();
+	    map.put("key", "value");
+	    map.put("foo", "bar");
+	    map.put("aa", "bb");
+	    return map;
 	}
+	
+	@GetMapping("/employees2")
+	public String helloWorld()
+	 {	     // Print statement
+	     return "Hello World!";
+	 }
+	
+	
 }
